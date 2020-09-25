27e4a0d769db3c71afb9b057b3d80ce6761137d5
Amir <tliamy@ubuntu-20.04.1-desktop-amd64>
2020-09-25 17:41:20 +0800

    initial commit

---
+++ pong.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const winWidth, winHeight int = 800, 600
+
+type colour struct {
+	r, g, b byte
+}
+
+type pos struct {
+	x, y float32
+}
+
+type ball struct {
+	pos
+	radius int
+	xv     float32
+	yv     float32
+	colour colour
+}
+
+func (ball *ball) draw(pixels []byte) {
+	for y := -ball.radius; y < ball.radius; y++ {
+		for x := -ball.radius; x < ball.radius; x++ {
+			if x*x+y*y < ball.radius*ball.radius {
+				setPixel(int(ball.x)+x, int(ball.y)+y, ball.colour, pixels)
+			}
+		}
+	}
+}
+
+type paddle struct {
+	pos
+	w      int
+	h      int
+	colour colour
+}
+
+func (paddle *paddle) draw(pixels []byte) {
+	startX := int(paddle.x) - paddle.w/2
+	startY := int(paddle.y) - paddle.h/2
+
+	for y := 0; y < paddle.h; y++ {
+		for x := 0; x < paddle.w; x++ {
+			setPixel(startX+x, startY+y, paddle.colour, pixels)
+		}
+	}
+}
+
+func setPixel(x, y int, c colour, pixels []byte) {
+	index := (y*winWidth + x) * 4
+	if index < len(pixels)-4 && index >= 0 {
+		pixels[index] = c.r
+		pixels[index+1] = c.g
+		pixels[index+2] = c.b
+	}
+}
+
+func main() {
+
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("Testing SDL2", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, int32(winWidth), int32(winHeight), sdl.WINDOW_SHOWN)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer renderer.Destroy()
+
+	tex, err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(winWidth), int32(winHeight))
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tex.Destroy()
+
+	pixels := make([]byte, winWidth*winHeight*4)
+	player1 := paddle{pos{100, 100}, 20, 100, colour{255, 255, 255}}
+	ball := ball{pos{300, 300}, 20, 0, 0, colour{255, 255, 255}}
+
+	for {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				return
+			}
+		}
+		player1.draw(pixels)
+		ball.draw(pixels)
+
+		tex.Update(nil, pixels, winWidth*4)
+		renderer.Copy(tex, nil, nil)
+		renderer.Present()
+		sdl.Delay(16)
+	}
+}
+
+///// 26:00 @ 7
